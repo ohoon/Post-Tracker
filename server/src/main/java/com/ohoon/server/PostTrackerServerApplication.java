17d5c2a32843c67cf6ed8302b3aa035e969c8274
ohoon <0hoon5661@gmail.com>
2023-09-19 19:38:22 +0900

    Initial Commit

---
+++ server/src/main/java/com/ohoon/server/PostTrackerServerApplication.java
@@ -0,0 +1,13 @@
+package com.ohoon.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostTrackerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostTrackerServerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/ohoon/server/PostTrackerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohoon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostTrackerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
