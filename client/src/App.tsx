df73fcff7372176718ff49fa1d6978a4325df2df
ohoon <0hoon5661@gmail.com>
2023-09-26 18:53:00 +0900

    css 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -12,7 +11,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Post Tracker</h1>
         <Input />
         <hr/>
         <OutputControl/>
