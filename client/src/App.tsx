16b434c66eb051d6a57e0ebc1936192ed6adbb6a
ohoon <0hoon5661@gmail.com>
2024-05-29 17:17:19 +0900

    빈 상태에도 Output 컴포넌트 보이게 수정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,29 +3,20 @@ import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { useAppSelector } from "./app/hooks";
-import {
-    selectTrackInfoDict,
-} from "./features/track_info/trackInfoSlice";
-
 import { Input } from "./components/track_info/Input";
 import { Output } from './components/track_info/Output';
 import { OutputControl } from "./components/track_info/OutputControl";
 import { Footer } from "./components/footer/Footer";
 
 function App() {
-  const trackInfoDict = useAppSelector(selectTrackInfoDict);
-
   return (
     <div className="App">
       <header className="App-header">
         <h1>Post Tracker</h1>
         <Input />
-        {Object.keys(trackInfoDict).length > 0 && <>
           <hr/>
           <OutputControl/>
           <Output />
-        </>}
       </header>
       <Footer />
     </div>
